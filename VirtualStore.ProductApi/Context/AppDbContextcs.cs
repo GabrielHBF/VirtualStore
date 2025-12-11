8fa02ac556d5d8f2bbcc9b72e9fca75078242870
Gabriel <henrique.biel9753@gmail.com>
2025-12-11 15:28:47 -0300

    Adicionar arquivos de projeto.

---
+++ VirtualStore.ProductApi/Context/AppDbContextcs.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualStore.ProductApi.Model;
+
+namespace VirtualStore.ProductApi.Context;
+public class AppDbContextcs : DbContext
+{
+    public AppDbContextcs(DbContextOptions<AppDbContextcs> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+
+    //fluent api serve para configurar as entidades
+    //assim como as propriedades delas e seus relacionamentos
+    //nesse caso estamos configurando a entidade Category
+    //assim quando fizermos uma migration ela vai criar a tabela 
+    // de Category conforme essa configuração
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       //category
+       modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+       modelBuilder.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+        //product
+        modelBuilder.Entity<Product>().Property(p => p.Name).
+            HasMaxLength(100).
+            IsRequired();
+
+        modelBuilder.Entity<Product>().Property(p => p.Price).
+            HasColumnType("decimal(10,2)").
+            IsRequired();
+
+        modelBuilder.Entity<Product>().Property(p => p.Description).
+            HasMaxLength(255);
+
+        modelBuilder.Entity<Product>().Property(p => p.ImageUrl).
+            HasMaxLength(255).
+            IsRequired();
+
+        modelBuilder.Entity<Category>().
+            HasMany(c => c.Products).
+            WithOne(p => p.Category).
+            IsRequired().
+            OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, Name = "Electronics" },
+            new Category { CategoryId = 2, Name = "Books" },
+            new Category { CategoryId = 3, Name = "Clothing" }
+        );
+    }
+}
---
+++ VirtualStore.ProductApi/Migrations/20251211181920_SeedProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualStore.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Products (Name, Price, Description, Stock, ImageUrl, CategoryId) " +
+                "Values ('Smartphone XYZ', 699.99, 'Latest model with advanced features', 50, 'http://example.com/images/smartphone_xyz.jpg', 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Products");
+        }
+    }
+}
